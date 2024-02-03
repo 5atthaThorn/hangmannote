7d43aec945a768c8e28915fdd8af7b28e5d1ff7f
satthathorn056_1 <satthathorn.s@kkumail.com>
2024-02-04 02:06:17 +0700

    Fix bug

--- hangman/src/main/java/com/hangman/Controllers/restController.java
+++ hangman/src/main/java/com/hangman/Controllers/restController.java
@@ -2,14 +2,23 @@ package com.hangman.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 import com.hangman.Entities.Scores;
 import com.hangman.Entities.Users;
 import com.hangman.Repositories.ScoresRepository;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSessionEvent;
 
 @RestController
 @RequestMapping("/api")
@@ -33,3 +42,4 @@ public class restController {
         return ResponseEntity.ok("Score has been saved.");
     }
 }
+
